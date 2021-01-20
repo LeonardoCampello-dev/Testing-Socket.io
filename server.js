ccb6c8a39e57f42a1a9ab0ca6aa7abcd5a43f5f6
Leonardo Campello <leonardocampello.dev@gmail.com>
2021-01-20 09:06:05 -0300

    New indentation standard

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
-const app = express()
-const server = require('http').createServer(app)
-const io = require('socket.io')(server)
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'public'))
-app.engine('html', require('ejs').renderFile)
-app.set('view engine', 'html')
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "public"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
 
-app.use('/', (req, res) => {
-    res.render('index.html')
-})
+app.use("/", (req, res) => {
+  res.render("index.html");
+});
 
-let messages = []
+let messages = [];
 
-io.on('connection', socket => {
-    console.log(`Socket conectado: ${socket.id}`)
+io.on("connection", (socket) => {
+  console.log(`Socket conectado: ${socket.id}`);
 
-    socket.emit('previousMessages', messages)
+  socket.emit("previousMessages", messages);
 
-    socket.on('sendMessage', data => {
-        messages.push(data)
+  socket.on("sendMessage", (data) => {
+    messages.push(data);
 
-        socket.broadcast.emit('receivedMessage', data)
-    })
-})
+    socket.broadcast.emit("receivedMessage", data);
+  });
+});
 
-server.listen(3000)
+server.listen(3000);
